32f8eae52aa0095516b55adb0fa45cd8f4db4398
291317087@qq.com <291317087@qq.com>
2021-04-11 00:34:05 +0800

    add:AQuene

---
+++ AQuene/AQuene/AQuene.c
@@ -0,0 +1,165 @@
+﻿#include<stdio.h>
+#include <stdlib.h>
+#include"AQuene.h"
+
+
+//显示菜单
+inline void ShowMenu()
+{
+	printf_s("**************************************\n");
+	printf_s("************0.销毁队列****************\n");
+	printf_s("************1.检查队列是否已满********\n");
+	printf_s("************2.检查队列是否为空********\n");
+	printf_s("************3.查看队头元素************\n");
+	printf_s("************4.队列的长度**************\n");
+	printf_s("************5.入队********************\n");
+	printf_s("************6.出队********************\n");
+	printf_s("************7.遍历队列****************\n");
+	printf_s("************8.清空队列****************\n");
+	printf_s("**************************************\n");
+}
+
+//初始化队列
+inline void InitAQueue(AQueue* Q)
+{
+	int i;
+	Q->length = MAXQUEUE;
+	for (i = 0; i < MAXQUEUE; i++) {
+		Q->data[i] = (void*)malloc(Typesize);
+	}
+	Q->front = Q->rear = 0;
+
+}
+
+//销毁队列
+inline void DestoryAQueue(AQueue* Q)
+{
+	for (int i = 0; i < MAXQUEUE; i++) {
+		free(Q->data[i]);
+	}
+	Q->data[0] = NULL;
+}
+
+//检查队列是否已满
+inline Status IsFullAQueue(const AQueue* Q)
+{
+	if (LengthAQueue(Q) == MAXQUEUE - 1)
+	{
+		return TRUE;
+	}
+	else
+		return FALSE;
+
+}
+
+//判断队列是否为空
+inline Status IsEmptyAQueue(const AQueue* Q)
+{
+	if (LengthAQueue(Q) == 0)
+	{
+		return TRUE;
+	}
+	else
+	{
+		return FALSE;
+	}
+}
+
+
+
+
+//查看队头元素
+inline Status GetHeadAQueue(AQueue* Q, void* e)
+{
+	if (IsEmptyAQueue(Q))
+	{
+		printf_s("队列为空,不存在队头元素！\n");
+		return FALSE;
+	}
+	// 直接传值的地址
+	e = Q->data[Q->front];
+	return TRUE;
+}
+
+
+//确定队列长度
+inline int LengthAQueue(AQueue* Q)
+{
+	return (Q->rear - Q->front + Q->length) % Q->length;
+}
+
+//入队
+inline Status EnAQueue(AQueue* Q, void* data)
+{
+
+	if (IsFullAQueue(Q))
+	{
+		printf_s("队列已满，无法进行入队操作!\n");
+		return FALSE;
+	}
+	if (IsEmptyAQueue(Q))
+	{
+		memcpy(Q->data[Q->front], data, Typesize);
+		Q->rear = (Q->rear + 1) % Q->length;
+		printf("入队成功！");
+		return TRUE;
+	}
+	memcpy(Q->data[Q->rear], data, Typesize);
+	Q->rear = (Q->rear + 1) % Q->length;
+	printf("入队成功！");
+	return TRUE;
+}
+
+//出队
+inline Status DeAQueue(AQueue* Q)
+{
+	if (IsEmptyAQueue(Q))
+	{
+		printf_s("队列为空，无法进行出队操作！\n");
+		return FALSE;
+	}
+	Q->front = (Q->front + 1) % Q->length;
+	printf("出队成功！");
+	return TRUE;
+}
+
+//清空队列
+inline void ClearAQueue(AQueue* Q)
+{
+	if (IsEmptyAQueue(Q))
+	{
+		printf_s("队列为空，不用进行清空操作！\n");
+		return;
+	}
+	Q->front = Q->rear = 0;
+}
+
+//遍历函数操作
+inline Status TraverseAQueue(const AQueue* Q, void (*foo)(void* q))
+{
+	if (IsEmptyAQueue(Q))
+	{
+		printf_s("队列为空!\n");
+		return FALSE;
+	}
+	int i;
+	for (int i = Q->front, j = 0; j <LengthAQueue(Q); j++)
+	{
+		foo(Q->data[i]);
+		printf(" ");
+		i = (i + 1) % MAXQUEUE;
+	}
+	return TRUE;
+}
+
+//操作函数
+inline void APrint(void* q)
+{
+	if (type == 'i')
+		printf_s("%d", *((int*)q));
+	else if (type == 'd')
+		printf_s("%.2lf", *((double*)q));
+	else if (type == 'c')
+		printf_s("%c", *((char*)q));
+
+}
---
+++ AQuene/AQuene/Main.c
@@ -0,0 +1,146 @@
+﻿#include<stdio.h>
+#include <stdlib.h>
+#include"AQuene.h"
+#include"AQuene.c"
+
+
+int main()
+{
+	int select, choice;
+	void* data=NULL;
+	AQueue* Q;
+	Q = (AQueue*)malloc(sizeof(AQueue));
+
+	printf_s("现在先确定队列要存取哪种数据类型: \n");
+	printf_s(" 1.int型\t 2.double型\t 3.char型\n");
+	scanf_s("%d", &select);
+	(int)select;
+	while (1)
+	{
+		if (select == 1)
+		{
+			type = 'i';
+			Typesize = sizeof(int);
+			break;
+		}
+		else if (select == 2)
+		{
+			type = 'd';
+			Typesize = sizeof(double);
+			break;
+		}
+		else if (select == 3)
+		{
+			type = 'c';
+			Typesize = sizeof(char);
+			break;
+		}
+		printf_s("无所输选项，无法进行操作！\n");
+		break;
+	}
+
+	if (select == 1 || select == 2 || select == 3)
+	{
+		InitAQueue(Q);
+		printf_s("已为您初始化好队列！\n");
+		while (1)
+		{
+			ShowMenu();
+			printf_s("请输入你的选项：\n");
+			scanf_s("%d", &choice);
+			(int)choice;
+			switch (choice)
+			{
+			case 0:
+				DestoryAQueue(Q);
+				printf_s("队列已销毁！\n");
+				break;
+			case 1:
+				if (IsFullAQueue(Q))
+				{
+					printf_s("队列已满！\n");
+				}
+				else
+				{
+					printf_s("队列未满！\n");
+				}
+				break;
+			case 2:
+				if (IsEmptyAQueue(Q))
+				{
+					printf("队列为空！\n");
+				}
+				else
+				{
+					printf_s("队列不为空！\n");
+				}
+				break;
+			case 3:
+			{
+				GetHeadAQueue(Q, &data);
+				if (select == 1)
+				{
+					printf_s("队头元素为：%d", *(int*)data);
+				}
+				else if (select == 2)
+				{
+					printf_s("队头元素为：%.2lf", *(double*)data);
+				}
+				else if (select == 3)
+				{
+					printf_s("队头元素为：%c", *(char*)data);
+				}
+				break;
+			}
+			case 4:
+			{
+				if (IsEmptyAQueue(Q))
+				{
+					printf_s("队列为空。\n");
+				}
+				else {
+					printf_s("%d", LengthAQueue(Q));
+				}
+				break;
+			}
+			case 5:
+			{
+				printf_s("请输入要入队的数值：\n");
+				if (select == 1)
+				{
+					data = (int*)malloc(Typesize);
+					scanf_s("%d", (int*)data);
+				}
+				else if (select == 2)
+				{
+					data = (double*)malloc(Typesize);
+					scanf_s("%lf", (double*)data);
+				}
+				else if (select == 3)
+				{
+					data = (char*)malloc(Typesize);
+					getchar();//忽略回车字符
+					scanf_s("%c", (char*)data, 1);
+				}
+				EnAQueue(Q, data);
+				break;
+			}
+			case 6:
+				DeAQueue(Q);
+				break;
+			case 7:
+				TraverseAQueue(Q, APrint);
+				break;
+			case 8:
+				ClearAQueue(Q);
+				break;
+			default:
+				printf_s("输入有误，无可执行功能！\n");
+				break;
+			}
+			system("pause");
+			system("cls");
+		}
+	}
+	return 0;
+}
